5bc4da2757ea95e4649efb5d03ed9d006158530c
Hong <Chengchao.Hong@contractor.conocophillips.com>
2020-09-15 08:28:18 +0800

    First Commit

---
+++ src/build/precache-manifest.28f338b016a2dd5039a63ea5246d97c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec3b07f68d15280a64f4ea4dcf46af3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a527e0db7f96f1711bc",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a527e0db7f96f1711bc",
+    "url": "/static/js/main.bcfe4c03.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
